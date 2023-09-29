aa76f7c8fa4c5d3269ea013ca93b16899572c736
anujakamble <anujakamble@vctpune.co.in>
2023-09-29 13:28:16 +0530

    added list prog

---
+++ Demo/src/Example1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example1 {
+	public static void main(String[] args) {
+		List l=new ArrayList();
+		l.add(10);
+		l.add(20);
+		System.out.println(l);
+	}
+
+}
